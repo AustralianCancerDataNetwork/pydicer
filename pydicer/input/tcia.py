30b9f6c120b6961a09fa9c188c1020df266cff84
dalmouiee <d.almouiee@gmail.com>
2021-09-21 12:36:48 +1000

    Replaced TCIAInput implementation with Platipy's

--- pydicer/input/tcia.py
+++ pydicer/input/tcia.py
@@ -1,93 +1,44 @@
 import os
+import zipfile
+import tempfile
 import urllib.request, urllib.error, urllib.parse
 import urllib.request, urllib.parse, urllib.error
-from pydicer.input.base import InputBase
-import zipfile
+import json
+
 from pathlib import Path
+from platipy.dicom.download import tcia
 
-#
-# Refer https://wiki.cancerimagingarchive.net/display/Public/REST+API+Usage+Guide for complete
-# list of API
-#
-# Some of the code was adapted from the TCIA Python APi client example:
-# https://github.com/TCIA-Community/TCIA-API-SDK/blob/master/tcia-rest-client-python/src/tciaclient.py
-#
-
-
-def run_endpoint(url, query_parameters):
-    """
-    Execute the TCIA API endpoint
-
-    Args:
-        url (str): url of the series to be downloaded and generate its DICOMS
-        query_parameters (dict): dictionary of query parameters to be passed into the api
-        endpoint request.
-
-    Returns:
-        resp (file): the response file in bytes to be written into a zipfile
-    """
-
-    query_parameters = dict((k, v) for k, v in query_parameters.items() if v)
-    queryString = "?%s" % urllib.parse.urlencode(query_parameters)
-    requestUrl = url + queryString
-    request = urllib.request.Request(url=requestUrl)
-    resp = urllib.request.urlopen(request)
-    return resp
+from pydicer.input.base import InputBase
 
 
 class TCIAInput(InputBase):
-    def __init__(self, series_instance_uid, working_directory=None):
+    def __init__(self, collection, patient_ids, modalities=None, working_directory=None):
         """
         Input class that interfaces with the TCIA API
 
         Args:
-            series_instance_uid (str): The series Instance UID to be downloaded from TCIA
-            working_directory (str|pathlib.Path, optional): The working directory in which to
-            store the downloaded data. Defaults to a temp directory
+            collection (str): The TCIA collection to fetch from
+            patient_ids (list, optional): The patient IDs to fetch. If not set all patients are
+                                        fetched
+            modalities (list, optional): A list of strings defining the modalites to fetch. Will fetch
+                                        all modalities available if not specified.
+            working_directory (str): (str|pathlib.Path, optional): The working directory in which to
+            store the data fetched. Defaults to a temp directory.
         """
         super().__init__(working_directory)
-        self.prefix_url = "https://services.cancerimagingarchive.net/services/v4/TCIA"
-        self.series_instance_uid = series_instance_uid
+        self.collection = collection
+        self.patient_ids = patient_ids
+        self.modalities = modalities
 
-    def fetch_data(self, output_zip_file="tcia_input_zipfile.zip"):
+    def fetch_data(self):
         """
-        Function to download the Series instance UID from TCIA and write locally
-
-        Args:
-            output_zip_file (str): name of temporary zipfile
+        Function to download the data from TCIA and write locally
         """
 
-        serviceUrl = self.prefix_url + "/query/getImage"
-        query_parameters = {"SeriesInstanceUID": self.series_instance_uid}
-        try:
-            self.working_directory.mkdir(exist_ok=True, parents=True)
-            file = self.working_directory.joinpath(output_zip_file)
-            resp = run_endpoint(serviceUrl, query_parameters)
-
-            # If series instance uid doesn't exist in TCIA, return
-            if not resp.chunked:
-                print(
-                    "Error: The Series Instance UID provided does not exist, please confirm that it"
-                    " is correct..."
-                )
-                return
-
-            # Chunks to be read by, used by TCIA's official python API client example
-            CHUNK = 256 * 10240
-            # Open zipfile
-            with open(file, "wb") as fp:
-                # Keep reading bytes response file
-                while True:
-                    chunk = resp.read(CHUNK)
-                    # Stop reading and writing bytes file when there are no more bytes left
-                    if not chunk:
-                        break
-                    # Write chunk to zipfile
-                    fp.write(chunk)
-            # Unzip temporary zipfile with DICOMs
-            with zipfile.ZipFile(file, "r") as zip_ref:
-                zip_ref.extractall(self.working_directory)
-            # Remove temporary zipfile once the DICOMs are written
-            os.remove(file)
-        except Exception as e:
-            print(e)
+        tcia.fetch_data(
+            self.collection,
+            self.patient_ids,
+            self.modalities,
+            nifti=False,
+            output_directory=self.working_directory,
+        )
