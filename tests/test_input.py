3db64077461edfd78c48a04ebfbc26470713ee74
Phillip Chlap <phillip.chlap@unsw.edu.au>
2021-09-21 17:44:47 +1000

    Remove duplicate imports

--- tests/test_input.py
+++ tests/test_input.py
@@ -1,13 +1,11 @@
 import pytest
 import os
-import pytest
 
 from pydicer.input.web import WebInput, download_and_extract_zip_file
 from pydicer.input.test import TestInput
 from pydicer.input.filesystem import FilesystemInput
 from pydicer.input.pacs import DICOMPACSInput
 from pydicer.input.tcia import TCIAInput
-from pydicer.input.pacs import DICOMPACSInput
 
 
 def test_input_valid_working_dir():
