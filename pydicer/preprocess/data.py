630c7e651b88c5052de01b2f137c1db708d4c717
Phillip Chlap <phillip.chlap@unsw.edu.au>
2021-10-15 09:20:47 +1100

    Rework preprocess and convert to use a DataFrame Structure

--- pydicer/preprocess/data.py
+++ pydicer/preprocess/data.py
@@ -1,5 +1,7 @@
 import logging
 
+import pandas as pd
+
 import pydicom
 import numpy as np
 
@@ -29,44 +31,55 @@ class PreprocessData:
         """
         Function to preprocess information regarding the data located in an Input working directory
 
-        Returns: res_dict (dict): keys are series UIDs. For each series; we have 5 lower keys:
-            - the hashed patient ID
-            - the hashed study UID
-            - a list of dicts that has 2 keys (path to file, slice location)
-            - the series modality
-            - the series UID that it is linked to
+        Returns: res_dict (pd.DataFrame): containing a row for each DICOM file that was
+           preprocessed, with the following columns:
+            - patient_id: PatientID field from the DICOM header
+            - study_uid: StudyInstanceUID field from the DICOM header
+            - series_uid: SeriesInstanceUID field from the DICOM header
+            - modality: Modailty field from the DICOM header
+            - sop_class_uid: SOPClassUID field from the DICOM header
+            - for_uid: FrameOfReferenceUID field from the DICOM header
+            - file_path: The path to the file (as a pathlib.Path object)
+            - slice_location: The real-world location of the slice (used for imaging modalities)
+            - referenced_series_uid: The SeriesUID referenced by this DICOM file (used for RTSTRUCT
+              and RTDOSE)
+            - referenced_for_uid: The ReferencedFrameOfReferenceUID referenced by this DICOM file
 
-        Ex:
-            {
-                "series_uid": {
-                    "patient_id": "",
-                    "study_id": "",
-                    "files": [],
-                    "modality": "",
-                    "linked_series_uid": {}
-                }
-            }
         """
-        res_dict = {}
+        df = pd.DataFrame(
+            columns=[
+                "patient_id",
+                "study_uid",
+                "series_uid",
+                "modality",
+                "sop_class_uid",
+                "for_uid",
+                "file_path",
+                "slice_location",
+                "referenced_series_uid",
+                "referenced_for_uid",
+            ]
+        )
         files = self.working_directory.glob("**/*.dcm")
 
         for file in files:
             ds = pydicom.read_file(file, force=True)
 
-            linked_series_uid = {}
-
             try:
 
                 dicom_type_uid = ds.SOPClassUID
 
-                if ds.SeriesInstanceUID not in res_dict:
-                    res_dict[ds.SeriesInstanceUID] = {
-                        "patient_id": ds.PatientID,
-                        "study_id": ds.StudyInstanceUID,
-                        "files": [],
-                        "modality": ds.Modality,
-                        "sop_class_uid": dicom_type_uid,
-                    }
+                res_dict = {
+                    "patient_id": ds.PatientID,
+                    "study_uid": ds.StudyInstanceUID,
+                    "series_uid": ds.SeriesInstanceUID,
+                    "modality": ds.Modality,
+                    "sop_class_uid": dicom_type_uid,
+                    "file_path": str(file),
+                }
+
+                if "FrameOfReferenceUID" in ds:
+                    res_dict["for_uid"] = ds.FrameOfReferenceUID
 
                 if dicom_type_uid == RT_STRUCTURE_STORAGE_UID:
 
@@ -77,19 +90,20 @@ class PreprocessData:
                             .RTReferencedSeriesSequence[0]
                             .SeriesInstanceUID
                         )
-                        linked_series_uid["referenced_series_uid"] = referenced_series_uid
+                        res_dict["referenced_series_uid"] = referenced_series_uid
                     except AttributeError:
+                        logger.warning("Unable to determine Reference Series UID")
+
+                    try:
                         # Check other tags for a linked DICOM
                         # e.g. ds.ReferencedFrameOfReferenceSequence[0].FrameOfReferenceUID
                         # Potentially, we should check each referenced
                         referenced_frame_of_reference_uid = ds.ReferencedFrameOfReferenceSequence[
                             0
                         ].FrameOfReferenceUID
-                        linked_series_uid[
-                            "referenced_frame_of_reference_uid"
-                        ] = referenced_frame_of_reference_uid
-
-                    res_dict[ds.SeriesInstanceUID]["files"].append(file)
+                        res_dict["referenced_for_uid"] = referenced_frame_of_reference_uid
+                    except AttributeError:
+                        logger.warning("Unable to determine Referenced Frame of Reference UID")
 
                 elif dicom_type_uid == CT_IMAGE_STORAGE_UID:
 
@@ -100,32 +114,23 @@ class PreprocessData:
 
                     slice_location = (image_position * image_plane_normal)[2]
 
-                    temp_dict = {"path": file, "slice_location": slice_location}
-
-                    res_dict[ds.SeriesInstanceUID]["files"].append(temp_dict)
+                    res_dict["slice_location"] = slice_location
 
                 else:
                     raise ValueError("Could not determine DICOM type.")
+
+                # Add as a row to the DataFrame
+                df = df.append(res_dict, ignore_index=True)
 
             except Exception as e:  # pylint: disable=broad-except
                 # Broad except ok here, since we will put these file into a
                 # quarantine location for further inspection.
                 logger.error("Error parsing file %s: %s", file, e)
 
-            # Include any linked DICOM series
-            # This is a dictionary holding potential matching series
-            res_dict[ds.SeriesInstanceUID]["linked_series_uid"] = linked_series_uid
+                # TODO Send to quarantine
 
-        # Sort the files for each series by the slice_location (if available)
-        for _, value in res_dict.items():
+        # Sort the the DataFrame by the patient then series uid and the slice location, ensuring
+        # that the slices are ordered correctly
+        df = df.sort_values(["patient_id", "series_uid", "slice_location"])
 
-            if len(value["files"]) == 0:
-                continue
-
-            if not isinstance(value["files"][0], dict):
-                continue
-
-            if "slice_location" in value["files"][0]:
-                value["files"].sort(key=lambda x: x["slice_location"])
-
-        return res_dict
+        return df
