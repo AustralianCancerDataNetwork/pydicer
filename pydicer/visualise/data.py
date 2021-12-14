4be3cad81f6052ed05d0a85b68d4fbb3ef2c5318
Phillip Chlap <phillip.chlap@unsw.edu.au>
2021-12-14 18:43:23 +1100

    Initial code for conversion of RTDOSE modality

--- pydicer/visualise/data.py
+++ pydicer/visualise/data.py
@@ -1,6 +1,7 @@
 import logging
 from pathlib import Path
 import SimpleITK as sitk
+import matplotlib.pyplot as plt
 from platipy.imaging import ImageVisualiser
 
 logger = logging.getLogger(__name__)
@@ -79,6 +80,67 @@ class VisualiseData:
 
             # save image inside structure directory
             vis_filename = struct_dir.parent.joinpath(f"{struct_dir.name}.png")
+            fig.savefig(
+                vis_filename,
+                dpi=fig.dpi,
+            )
+
+            logger.debug("created visualisation: %s", vis_filename)
+
+        # Next visualise the doses on top of their linked image
+        for dose_file in self.output_directory.glob("**/doses/*.nii.gz"):
+
+            ## Currently doses are linked via: plan -> struct -> image
+
+            # Find the linked plan
+            plan_id = dose_file.name.replace(".nii.gz", "").split("_")[-1]
+            plan_links = list(dose_file.parent.parent.glob(f"plans/*{plan_id}_*.json"))
+
+            if len(plan_links) == 0:
+                logger.error("Linked plan %s not found", plan_id)
+                continue
+
+            # Find the linked struct
+            struct_id = plan_links[0].name.replace(".json", "").split("_")[-1]
+            struct_links = list(dose_file.parent.parent.glob(f"structures/{struct_id}_*/"))
+
+            if len(struct_links) == 0:
+                logger.error("Linked structure %s not found", struct_id)
+                continue
+
+            # Finally find the linked image
+            struct_dir = struct_links[0]
+            img_id = struct_dir.name.split("_")[1]
+            img_links = list(struct_dir.parent.parent.glob(f"images/*{img_id}.nii.gz"))
+
+            if len(img_links) == 0:
+                logger.error("Linked image %s not found", img_id)
+                continue
+
+            img_file = img_links[0]
+
+            img = sitk.ReadImage(str(img_file))
+            dose_img = sitk.ReadImage(str(dose_file))
+            dose_img = sitk.Resample(dose_img, img)
+
+            vis = ImageVisualiser(img)
+
+            vis.add_scalar_overlay(
+                dose_img, "Dose", discrete_levels=20, colormap=plt.cm.get_cmap("inferno")
+            )
+
+            masks = {
+                f.name.replace(".nii.gz", ""): sitk.ReadImage(str(f))
+                for f in struct_dir.glob("*.nii.gz")
+            }
+
+            vis.add_contour(masks)
+
+            fig = vis.show()
+
+            # save image inside doses directory
+            file_name = dose_file.name.replace(".nii.gz", ".png")
+            vis_filename = dose_file.parent.joinpath(f"{file_name}")
             fig.savefig(
                 vis_filename,
                 dpi=fig.dpi,
