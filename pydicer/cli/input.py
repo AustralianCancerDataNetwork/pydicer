764e4487acd8458d1806c596620fa7f5bb5303a8
dalmouiee <d.almouiee@gmail.com>
2021-12-16 18:01:20 +1100

    pylint complaining

--- pydicer/cli/input.py
+++ pydicer/cli/input.py
@@ -1,8 +1,9 @@
-import logging
 from pydicer.input.test import TestInput
 
 
-def testinput_cli(logger):
+def testinput_cli():
+    """Trigger the test input as a mini pipilien for the CLI tool
+    """
     print("Running Test Input Module")
     test_input = TestInput()
     test_input.fetch_data()
