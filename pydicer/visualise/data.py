ff3cfdd53efd2c6c85bdab18110eedf77a560c39
Phillip Chlap <pchlap@gmail.com>
2024-11-25 04:23:11 +1100

    Visualise masks only ignoring other files

--- pydicer/visualise/data.py
+++ pydicer/visualise/data.py
@@ -59,6 +59,7 @@ class VisualiseData:
         )
 
         visualise_modalities = ["CT", "MR", "RTSTRUCT", "RTDOSE", "PT"]
+        render_mask_types = [sitk.sitkUInt8, sitk.sitkUInt16, sitk.sitkUInt32, sitk.sitkUInt64]
         df_process = df_process[df_process.modality.isin(visualise_modalities)]
 
         for _, row in get_iterator(
@@ -203,6 +204,8 @@ class VisualiseData:
                         for f in struct_dir.glob("*.nii.gz")
                     }
 
+                    masks = {k: masks[k] for k in masks if masks[k].GetPixelID() in render_mask_types}
+
                     if len(masks) == 0:
                         logger.warning(
                             "No contours found in structure directory: %s", {struct_dir}
@@ -280,6 +283,7 @@ class VisualiseData:
                             f.name.replace(".nii.gz", ""): sitk.ReadImage(str(f))
                             for f in struct_dir.glob("*.nii.gz")
                         }
+                        masks = {k: masks[k] for k in masks if masks[k].GetPixelID() in render_mask_types}
 
                         if len(masks) == 0:
                             logger.warning(
